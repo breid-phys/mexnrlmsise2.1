b8e46a521b88caf32e6c3fac517fdd4c41144751
Ben <breid.phys@gmail.com>
2023-02-01 19:13:43 -0400

    cleanup, removed need to change directory

--- nrlmsise2_1.m
+++ nrlmsise2_1.m
@@ -4,7 +4,7 @@ function Out = nrlmsise2_1(lat, lon, time, alt, varargin)
 % INPUTS
 %   latitude(deg), longitude(deg), time(datenum or datetime), altitude(km)
 %   latitude and longitude must have the same size, unless the 'map'
-%   keyword if provided. 
+%   keyword if provided.
 %
 %   time must be a scalar.
 %
@@ -28,8 +28,8 @@ function Out = nrlmsise2_1(lat, lon, time, alt, varargin)
 % OPTIONAL INPUTS
 %
 %   By default, NRLMSISE works in profile mode. Lat and Lon must be the
-%   same size, and the output will calculate NRLMSISE2.1 at every altitude 
-%   for each Lat/Lon pair. 
+%   same size, and the output will calculate NRLMSISE2.1 at every altitude
+%   for each Lat/Lon pair.
 %
 %   'map' mode: in this mode, all unique values of inputs are expanded in a
 %   4d grid. The output Ne is of a size [n lat,n lon,n alt,n time]
@@ -44,8 +44,13 @@ function Out = nrlmsise2_1(lat, lon, time, alt, varargin)
 
 ErrorStr = 'MATLAB:NRLMSISE2_1';
 
-[ModelDir, ~, ~] = fileparts(mfilename('fullpath'));
-ModelDir = fullfile(ModelDir);
+% PATH TO MEX FILE
+[RootDir, ~, ~] = fileparts(mfilename('fullpath')); 
+
+% PATH TO NRLMSISE2.1 FORTRAN CODE
+ModelDir = fullfile(RootDir,'nrlmsise2.1');
+
+ModelDir = fullfile(ModelDir,filesep);
 
 if isempty(ModelDir)
     error(ErrorStr,[...
@@ -57,146 +62,155 @@ elseif ~exist(ModelDir,'dir')
         mfilename('fullpath'),' to specify the appropriate path'])
 end
 
-try
+
+
+if ~exist(fullfile(RootDir,'mex_nrlmsise2_1'),'file')
     OldDir = pwd;
     cd(ModelDir);
-    if ~exist('mex_nrlmsise2_1','file') 
-
-        mex -compatibleArrayDims -DO3 -Dstatic -DfPIC -Dffast-math ...
-            -Dmarch=native ...
-            -output mex_nrlmsise2_1 ...
-            msis_constants.F90 msis_utils.F90 msis_init.F90 msis_gfn.F90 ...
-            msis_tfn.F90 msis_dfn.F90 msis_calc.F90 mex_nrlmsise2_1.F
-    end
-
-    if numel(time) >1
-        error(ErrorStr,[...
-            'time must be a scalar']);
-    end
-
-    mode='default';
-    flux = 'file';
-    Update = false;
-    i=1;
-    while i <= numel(varargin)
-        if ischar(varargin{i}) || isstring(varargin{i})
-            if strcmpi(varargin{i},'default')
-                mode = default;
-            elseif strcmpi(varargin{i},'map')
-                mode='map';
-            elseif strcmpi(varargin{i},'flux')
-                flux = 'input';
-                i=i+1;
-                F107 = varargin{i};
-            elseif strcmpi(varargin{i},'update')
-                Update=true;
-            else
-                error(ErrorStr,[...
-                    'Invalid option: ',varargin{i}]);
-            end
-        else
-            error(ErrorStr,[...
-                'Invalid option.'])
-        end
-        i=i+1;
-    end
-
-    if Update % update apf107.dat file in iri directory
-        read_apf(fullfile(ModelDir,'apf107.dat'),true);
-    end
-
-    if any((lat(:)<-90) | (lat(:)>90))
-        error(ErrorStr,[...
-            'Input latitude out of range [-90,90]'])
-    end
-
-    if any((lon(:)<-180) | (lon(:)>360))
-        error(ErrorStr,[...
-            'Input longitude out of range [-180,360]'])
-    end
-
-    APF = read_apf(fullfile(ModelDir,'apf107.dat'));
-
-    idate = sum(datenum(APF.date(:)')<datenum(time(:))',2);
-
-    if strncmpi(mode,'default',3)
-
-        tvec=[numel(lat),numel(lon)];
-        tvec=tvec==tvec';
-        if ~all(tvec(:))
-            error('In "default" mode and lat, lon  must all have the same size')
-        end
-
-        Out.time = time(:);
-        Out.lon = lon;
-        Out.lat = lat;
-        Out.alt = alt(:);
-
-        OutFmt = @(X) reshape(X,[numel(alt),size(lon)]);
-        [~,lat] = ndgrid(alt,lat);
-        [alt, lon] = ndgrid(alt,lon);
-
-    elseif strncmpi(mode,'map',3)
-
-        lat = unique(lat);
-        lon = unique(lon);
-        alt = unique(alt);
-
-        [Out.lat,Out.lon] = ndgrid(lat,lon);
-        Out.alt = alt(:);
-        Out.time = time;
-
-        OutFmt = @(X) reshape(X,numel(alt), numel(lat), numel(lon));
-        [alt,lat,lon] = ndgrid(alt,lat(:),lon(:));
+    try
+        mex('-compatibleArrayDims','-DO3','-Dstatic','-DfPIC', ...
+            '-Dffast-math','-Dmarch=native', ...
+            '-output',fullfile(RootDir,'mex_nrlmsise2_1'), ...
+            fullfile(ModelDir,'msis_constants.F90'), ...
+            fullfile(ModelDir,'msis_utils.F90'), ...
+            fullfile(ModelDir,'msis_init.F90'),...
+            fullfile(ModelDir,'msis_gfn.F90'), ...
+            fullfile(ModelDir,'msis_tfn.F90'), ...
+            fullfile(ModelDir,'msis_dfn.F90'), ...
+            fullfile(ModelDir,'msis_calc.F90'),...
+            fullfile(RootDir,'mex_nrlmsise2_1.F'))
+        cd(OldDir)
+    catch err
+        cd(OldDir)
+        rethrow(err)
 
     end
-    [Year,~,~,~,~,~] = datevec(time(:));
 
-    IYD = floor(datenum(time(:))-datenum(Year,0,0));
-    SEC = mod(datenum(time(:)),1)*24*60*60;
-    F107A = APF.F107_81(idate(:));
-    
-    % overwrite f107from file
-    if isnumeric(flux)
-   
-    else
-        F107 = APF.F107(idate(:));
-    end
-
-    Ap = APF.Ap(idate(:));
-    SW = ones(25,1);
-
-
-    [D,T] = mex_nrlmsise2_1( ...
-        IYD,SEC,F107A,F107,Ap,SW, ...
-        [reshape(alt,1,[]); ...
-        reshape(lat,1,[]); ...
-        reshape(lon,1,[])]);
-
-    Out.Total = OutFmt(D(1,:));
-    Out.N2 = OutFmt(D(2,:));
-    Out.O2 = OutFmt(D(3,:));
-    Out.O = OutFmt(D(4,:));
-    Out.He = OutFmt(D(5,:));
-    Out.H = OutFmt(D(6,:));
-    Out.Ar = OutFmt(D(7,:));
-    Out.N = OutFmt(D(8,:));
-    Out.Oa = OutFmt(D(9,:));
-    Out.NO = OutFmt(D(10,:));
-
-    Out.TEx = OutFmt(T(2,:));
-    Out.TN = OutFmt(T(1,:));
-    Out.F107 = F107;
-    Out.F107A = F107A;
-    Out.Ap = Ap;
-
-    cd(OldDir)
-
-catch err
-    cd(OldDir)
-    rethrow(err)
 
 end
+
+if numel(time) >1
+    error(ErrorStr,[...
+        'time must be a scalar']);
+end
+
+mode='default';
+flux = 'file';
+Update = false;
+i=1;
+while i <= numel(varargin)
+    if ischar(varargin{i}) || isstring(varargin{i})
+        if strcmpi(varargin{i},'default')
+            mode = default;
+        elseif strcmpi(varargin{i},'map')
+            mode='map';
+        elseif strcmpi(varargin{i},'flux')
+            flux = 'input';
+            i=i+1;
+            F107 = varargin{i};
+        elseif strcmpi(varargin{i},'update')
+            Update=true;
+        else
+            error(ErrorStr,[...
+                'Invalid option: ',varargin{i}]);
+        end
+    else
+        error(ErrorStr,[...
+            'Invalid option.'])
+    end
+    i=i+1;
+end
+
+if Update % update apf107.dat file in iri directory
+    read_apf(fullfile(RootDir,'apf107.dat'),true);
+end
+
+if any((lat(:)<-90) | (lat(:)>90))
+    error(ErrorStr,[...
+        'Input latitude out of range [-90,90]'])
+end
+
+if any((lon(:)<-180) | (lon(:)>360))
+    error(ErrorStr,[...
+        'Input longitude out of range [-180,360]'])
+end
+
+APF = read_apf(fullfile(RootDir,'apf107.dat'));
+
+idate = sum(datenum(APF.date(:)')<datenum(time(:))',2);
+
+if strncmpi(mode,'default',3)
+
+    tvec=[numel(lat),numel(lon)];
+    tvec=tvec==tvec';
+    if ~all(tvec(:))
+        error('In "default" mode and lat, lon  must all have the same size')
+    end
+
+    Out.time = time(:);
+    Out.lon = lon;
+    Out.lat = lat;
+    Out.alt = alt(:);
+
+    OutFmt = @(X) reshape(X,[numel(alt),size(lon)]);
+    [~,lat] = ndgrid(alt,lat);
+    [alt, lon] = ndgrid(alt,lon);
+
+elseif strncmpi(mode,'map',3)
+
+    lat = unique(lat);
+    lon = unique(lon);
+    alt = unique(alt);
+
+    [Out.lat,Out.lon] = ndgrid(lat,lon);
+    Out.alt = alt(:);
+    Out.time = time;
+
+    OutFmt = @(X) reshape(X,numel(alt), numel(lat), numel(lon));
+    [alt,lat,lon] = ndgrid(alt,lat(:),lon(:));
+
+end
+[Year,~,~,~,~,~] = datevec(time(:));
+
+IYD = floor(datenum(time(:))-datenum(Year,0,0));
+SEC = mod(datenum(time(:)),1)*24*60*60;
+F107A = APF.F107_81(idate(:));
+
+% overwrite f107from file
+if isnumeric(flux)
+
+else
+    F107 = APF.F107(idate(:));
+end
+
+Ap = APF.Ap(idate(:));
+SW = ones(25,1);
+
+
+[D,T] = mex_nrlmsise2_1( ...
+    IYD,SEC,F107A,F107,Ap,SW, ...
+    [reshape(alt,1,[]); ...
+    reshape(lat,1,[]); ...
+    reshape(lon,1,[])], ...
+    ModelDir);
+
+Out.Total = OutFmt(D(1,:));
+Out.N2 = OutFmt(D(2,:));
+Out.O2 = OutFmt(D(3,:));
+Out.O = OutFmt(D(4,:));
+Out.He = OutFmt(D(5,:));
+Out.H = OutFmt(D(6,:));
+Out.Ar = OutFmt(D(7,:));
+Out.N = OutFmt(D(8,:));
+Out.Oa = OutFmt(D(9,:));
+Out.NO = OutFmt(D(10,:));
+
+Out.TEx = OutFmt(T(2,:));
+Out.TN = OutFmt(T(1,:));
+Out.F107 = F107;
+Out.F107A = F107A;
+Out.Ap = Ap;
+
 
 
 
--- readme.txt
+++ readme.txt
@@ -1,3 +1,14 @@
+A MATLAB MEX wrapper for the NRLMSISE2.1 Empirical Model
+
+A MEX file allows users to execute FORTRAN code from within MATLAB.
+This repository adds two MATLAB function, one to interface with the MEX code, 
+the other to automatically download the apf107.dat file to provide needed indices
+to MSIS. 
+
+nrlmsise2_1.m should automatically compile a MEX file when first run.
+
+See nrlmsise2_1.m for details.
+
 #######################################################################
  MSIS® (NRL-SOF-014-1) SOFTWARE
  NRLMSIS® empirical atmospheric model software. Use is governed by the
@@ -26,79 +37,5 @@ REFERENCE FOR NRLMSIS 2.0
   of temperature and neutral species densities. Earth and Space Science, 8, 
   e2020EA001321. https://doi.org/10.1029/2020EA001321
 
-PACKAGE CONTENTS
-  readme.txt                This file
-  nrlmsis2.1_license.txt    Open Source Academic Research License Agreement
-  msis2.1_test.F90          Test program
-  msis_init.F90             Subroutines to initialize the model, set switches and
-                              options, and load parameter file
-  msis_gtd8d.F90            Subroutine to evaluate the model using the legacy
-                              interface
-  msis_calc.F90             Subroutine for evaluating the model using the new
-                              interface
-  msis_constants.F90        Module containing model constants
-  msis_gfn.F90              Subroutines to calculate horizontal expansion
-                              functions
-  msis_tfn.F90              Subroutines to calculate the vertical temperature
-                              profile
-  msis_dfn.F90              Subroutines to calculate vertical density profiles
-  msis_utils.F90            Subroutines to convert between geodetic height and
-                              geopotential height, and other support subroutines
-  msis21.parm               Binary data file containing model parameters
-  msis2.1_test_in.txt       ASCII file containing input for test program.
-  msis2.1_test_ref_dp.txt   ASCII file containing expected output of test program
-                             (double-precision internally)
- 
-RELEASE NOTES: MODEL FORMULATION
-  Minor changes to the NRLMSIS 2.0 formulation include:
-  - Addition of new terms to support fitting of NO densities.
-  - Reorganization of support subroutines (alt2gph, gph2alt, bspline, dilog)
-    into msis_utils module.
- 
-RELEASE NOTES: PARAMETER ESTIMATION
-  - NO density parameters were tuned to six NO data sets (ENVISAT/MIPAS, SNOE,
-    ACE/FTS, AIM/SOFIE, UARS/HALOE, and Odin/SMR).
-  - Temperature and all other species densities are the same as in NRLMSIS 2.0.
-
-COMPILING THE MODEL CODE
-  The model package was tested on Windows, Linux, and Mac systems using the
-  following Fortran compilers and compile statements:
-    gfortran 4.8.5, 7.5.0, 9.3.0
-      gfortran -O3 -cpp -o msis2.1_test.exe msis_constants.F90 msis_utils.F90
-      msis_init.F90 msis_gfn.F90 msis_tfn.F90 msis_dfn.F90 msis_calc.F90
-      msis_gtd8d.F90 msis2.1_test.F90
-        NOTES:
-        - The following optimization flags may improve performance:
-            -march=native -ffast-math
-    Intel 2017.2.163, 18.0.1.156, 2021.1
-      ifort -O2 -fpp -o msis2.1_test.exe msis_constants.F90 msis_utils.F90
-      msis_init.F90 msis_gfn.F90 msis_tfn.F90 msis_dfn.F90 msis_calc.F90
-      msis_gtd8d.F90 msis2.1_test.F90
-        NOTES:
-        - The following optimization flags may improve performance:
-            Windows:     -Qipo -QxHost
-            Linux/macOS: -ipo -xHost
-  For double precision, add the flag -DDBLE. Double precision is not necessary
-  for most applications, but for testing purposes it ensures that the test
-  output exactly matches the expected output in msis2.1_test_ref_dp.txt,
-  regardless of the compiler or compiler settings.
-
-INITIALIZING AND RUNNING THE MODEL
-  - The model must be initialized using the MSISINIT subroutine, which sets
-    switches and options and loads the model parameter values from a file.
-  - The switch_legacy optional argument to MSISINIT performs the same function
-    as TSELEC(SW) in NRLSMSISE-00, except that switches 15-25 are not used in
-    NRLMSIS 2.1. The change in the switch-setting call is illustrated as
-    follows, where SW is the 25-element array of switches:
-        NRLMSISE-00: CALL TSELEC(SW)
-        NRLMSIS 2.1: call msisinit(switch_legacy=SW)
-  - The MSISCALC subroutine checks for initialization and does a default
-    initialization if necessary. This self-initialization will be removed in
-    future versions.
-  - The model can be called using either the legacy interface (subroutine
-    GTD8D) or the new interface (subroutine MSISCALC).
-  - Details of the input and output arguments of MSISINIT, GTD8D, and MSISCALC
-    are provided in the headers of the respective source code files.
-  
 ACKNOWLEDGEMENTS
   This work was supported by the Office of Naval Research and NASA.
